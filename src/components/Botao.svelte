dc4ad7e66ceae399807336bb11b23f9072a59a66
LucasAmarilla <amarilla482@gmail.com>
2023-04-06 16:55:17 -0400

    first commit

---
+++ src/components/Botao.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let texto: string;
+    export let triplo: boolean = false;
+    export let duplo: boolean = false;
+    export let op: boolean = false;
+    export let destaque: boolean = false;
+    export let onClick: (valor: string) => void = () => {}
+
+</script>
+
+<button 
+    on:click={()=>onClick(texto)}
+    class="botao" class:triplo class:duplo class:op class:destaque >
+    {texto}
+</button>
+
+<style>
+.botao{
+    margin: 0;
+    border: 1px solid var(--back);
+    flex-basis: 25%;
+}
+.duplo{
+    flex-basis: 50%;
+}
+.triplo{
+    flex-basis: 75%;
+}
+.op{
+    background-color: chocolate;
+    color: white;
+}
+.destaque{
+    background-color: red;
+    color: white;
+}
+</style>
---
+++ src/components/Calculadora.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Tela from './Tela.svelte';
+    import Botao from "./Botao.svelte";
+    import Linha from "./Linha.svelte";
+    
+    let valor = '0';
+
+
+
+    function numeroDigitado(numero: string){
+        valor += numero
+    }
+
+</script>
+
+<div class="calculadora">
+    <Tela valor = {valor}/>
+    <Linha>
+        <Botao texto="AC" triplo destaque />
+        <Botao texto="/" op />
+        
+    </Linha>
+    <Linha>
+        <Botao texto="7" onClick={numeroDigitado}/>
+        <Botao texto="8" onClick={numeroDigitado}/>
+        <Botao texto="9" onClick={numeroDigitado}/>
+        <Botao texto="*" op/>
+    </Linha>
+    <Linha>
+        <Botao texto="6" />
+        <Botao texto="5" />
+        <Botao texto="4" />
+        <Botao texto="+" op/>
+    </Linha>
+    <Linha>
+        <Botao texto="1" />
+        <Botao texto="2" />
+        <Botao texto="3" />
+        <Botao texto="-" op/>
+    </Linha>
+    <Linha>
+        <Botao texto="0" duplo/>
+        <Botao texto="," />
+        <Botao texto="=" destaque/>
+    </Linha>
+</div>
+
+<style>
+    .calculadora {
+        height: 220px;
+        width: 150px;
+        padding: 10px;
+        border-radius: 10px;
+        background-color: var(--back);
+
+        display: flex;
+        flex-direction: column;
+    }
+</style>
